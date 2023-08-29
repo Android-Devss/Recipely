74d6169b3269c8e93d7a629169bc31d1075e7fd8
amanymohamed137 <93156418+amanymohamed137@users.noreply.github.com>
2023-08-29 04:16:38 +0300

    add home fragment adapter and design and add DataManager class

---
+++ app/src/main/java/com/example/recipely/ui/homeFragment/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.recipely.ui.homeFragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recipely.databinding.FragmentHomeBinding
+import com.example.recipely.ui.base.BaseFragment
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding =
+        FragmentHomeBinding::inflate
+
+    override val logTag: String = "RecipeFragment"
+
+    override fun initialize() {
+
+
+
+    }
+
+    override fun addCallbacks() {
+        // Add any required callbacks here
+    }
+}
+
