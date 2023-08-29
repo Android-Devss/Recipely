b1ed0176d423b89f18bcd65b41020ebc94913e6b
Maryam mohamed <mm3279@fayoum.edu.eg>
2023-08-30 00:59:47 +0300

    add recipe-details screen

--- app/src/main/java/com/example/recipely/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipely/ui/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.recipely.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.recipely.R
 import com.example.recipely.data.source.model.Recipe
 import com.example.recipely.data.source.DataSourceImp
 import com.example.recipely.databinding.ActivityMainBinding
 import com.example.recipely.util.CsvParser
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    override fun onCreate(savedInstanceState : Bundle?) {
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        showRecipeDetailsFragment()
     }
+    private fun showRecipeDetailsFragment() {
+        val recipeDetailsFragment = RecipeDetailsFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, recipeDetailsFragment)
+            .commit()
+    }
+
 }
