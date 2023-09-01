145e52b310e6360eaeb9d38d42b7df05af1e7e56
Omar Saeid <omarsaeid1911z@gmail.com>
2023-09-01 17:22:12 +0300

    add useCase search query

--- app/src/main/java/com/example/recipely/domain/Repository.kt
+++ app/src/main/java/com/example/recipely/domain/Repository.kt
@@ -1,5 +1,13 @@
 package com.example.recipely.domain
 
+import com.example.recipely.data.source.model.Advice
+import com.example.recipely.data.source.model.Recipe
+
 interface Repository {
+    fun getRecipes(): List<Recipe>
+    fun searchAboutRecipes(searchWord: String): List<Recipe>
+    fun getRecipe(id: Int): Recipe
+    fun getAdvices(): List<Advice>
+
 
 }
