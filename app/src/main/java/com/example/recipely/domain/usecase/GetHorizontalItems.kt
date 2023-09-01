cf930dab2a2240a94abc371dfc879bd5a1683dee
Maryam mohamed <mm3279@fayoum.edu.eg>
2023-09-01 15:59:19 +0300

    home fragment

--- app/src/main/java/com/example/recipely/domain/usecase/GetHorizontalItems.kt
+++ app/src/main/java/com/example/recipely/domain/usecase/GetHorizontalItems.kt
@@ -4,7 +4,7 @@ import com.example.recipely.data.source.model.Recipe
 import com.example.recipely.domain.Repository
 
 class GetHorizontalItems (private val repository: Repository){
-    operator fun invoke(id: Int):List<Recipe> {
-        return repository.getAllRecipes().take(id)
+    operator fun invoke(items: Int):List<Recipe> {
+        return repository.getAllRecipes().take(items)
     }
 }
