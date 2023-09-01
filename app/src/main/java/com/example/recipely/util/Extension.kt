8533ba10924cf654ae3714c3a37a03eff11f3aa5
amanymohamed13 <93275367+amanymohamed13@users.noreply.github.com>
2023-09-02 00:45:40 +0300

    Update Extension.kt
    add load image please holder

--- app/src/main/java/com/example/recipely/util/Extension.kt
+++ app/src/main/java/com/example/recipely/util/Extension.kt
@@ -3,6 +3,19 @@ package com.example.recipely.util
 import com.example.recipely.data.source.model.Recipe
 
 fun Int.toCountFormat() = "$this Items"
+fun ImageView.loadImageWithPlaceholderAndCrossFade(url: String) {
+    this.load(url) {
+        crossfade(1000)
+        placeholder(R.drawable.recipe_image_placeholder)
+        error(R.drawable.recipe_image_error)
+    }
+}
+
+fun Fragment.replaceFragment(fragment: Fragment) {
+    val fragmentManager = requireActivity().supportFragmentManager
+    val fragmentTransaction = fragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.fragment_container, fragment).addToBackStack(null)
+    fragmentTransaction.commit()
 
 fun Int.toTimeFormat() = "$this min"
 
