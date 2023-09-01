010682bf907519e637926e3a6057cfadd227db6c
Omar Saeid <omarsaeid1911z@gmail.com>
2023-09-01 17:29:26 +0300

    edit DataSource.kt and DataSourceImp.kt

--- app/src/main/java/com/example/recipely/data/source/DataSource.kt
+++ app/src/main/java/com/example/recipely/data/source/DataSource.kt
@@ -3,8 +3,7 @@ package com.example.recipely.data.source
 import com.example.recipely.data.source.model.Recipe
 
 interface DataSource {
-    fun getAllRecipes() : List<Recipe>
-
+    fun getAllRecipes(): List<Recipe>
 }
 
 
--- app/src/main/java/com/example/recipely/data/source/DataSourceImp.kt
+++ app/src/main/java/com/example/recipely/data/source/DataSourceImp.kt
@@ -8,21 +8,21 @@ import com.example.recipely.util.CsvParser
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-class DataSourceImp(private val context : Context, private val csvParser : CsvParser) : DataSource {
+class DataSourceImp(private val context: Context, private val csvParser: CsvParser) : DataSource {
 
-    override fun getAllRecipes() : List<Recipe> {
+    override fun getAllRecipes(): List<Recipe> {
+        var id = 0
         val recipeList = (mutableListOf<Recipe>())
         context.apply {
             val inputStream = assets.open(CSV_FILE_NAME)
             val buffer = BufferedReader(InputStreamReader(inputStream))
             buffer.forEachLine {
-                val currentRecipe = csvParser.parseLine(it)
+                val currentRecipe = csvParser.parseLine(it, id++)
                 recipeList.add(currentRecipe)
                 Log.v("Main_Activity", it)
             }
         }
         return recipeList
     }
-
 
 }
