0c15bcf1385d60e578b09e7febb9f30f1ea81012
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-09-02 03:38:04 +0300

    implement splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation("io.coil-kt:coil:$coilVersion")
     //Lottie
     implementation("com.airbnb.android:lottie:$lottieVersion")
+    //Splash Api
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/example/recipely/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipely/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recipely.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.recipely.R
 import com.example.recipely.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
