851a2884d8bf4377027f91e913e2f361acbed27d
Omar Saeid <omarsaeid1911z@gmail.com>
2023-09-03 18:59:51 +0300

    edit search

--- app/src/main/java/com/example/recipely/data/source/DataSource.kt
+++ app/src/main/java/com/example/recipely/data/source/DataSource.kt
@@ -1,11 +1,9 @@
 package com.example.recipely.data.source
 
-import com.example.recipely.data.source.model.Advice
 import com.example.recipely.data.source.model.Recipe
 
 interface DataSource {
     fun getAllRecipes(): List<Recipe>
-    fun getAdvices(): List<Advice>
 }
 
 
--- app/src/main/java/com/example/recipely/data/source/model/Advice.kt
+++
@@ -1,6 +0,0 @@
-package com.example.recipely.data.source.model
-
-data class Advice(
-    val imgUrl: String,
-    val description: String
-)
